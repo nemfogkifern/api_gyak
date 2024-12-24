3e536ae0cd94caff521d79273785d62eb7131074
Bercel Máté Gyurina <bercel.gyurina@stud.uni-corvinus.hu>
2024-12-24 14:58:51 +0100

    burger menu animation

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -32,22 +32,64 @@ fetch(apiUrl)
 
 
 // Burger Menu
-var nav = document.querySelector('nav');
+var menu = document.querySelector('.menu');
 var hamburger = document.querySelector('.hamburger');
 
 var topBar = document.querySelector('.bar-top');
 var middleBar = document.querySelector('.bar-middle');
 var bottomBar = document.querySelector('.bar-bottom');
 
+
 hamburger.addEventListener('click', function() {
-    nav.classList.toggle('active');
+    if (menu.classList.contains('active') && menu.classList.contains('active-reverse')) {
+        menu.classList.remove('active-reverse');
+    }
 
-    topBar.classList.toggle('bar-active-top');
-    middleBar.classList.toggle('bar-active-middle');
-    bottomBar.classList.toggle('bar-active-bottom');
+    else if (menu.classList.contains('active')) {
+        menu.classList.add('active-reverse');
+    }
+    else {
+        menu.classList.add('active');
 
-    topBar.classList.toggle('bar-active-top-reverse');
-    middleBar.classList.toggle('bar-active-middle-reverse');
-    bottomBar.classList.toggle('bar-active-bottom-reverse');
+    }
 
-});
+    if (topBar.classList.contains('bar-active-top-reverse') && topBar.classList.contains('bar-active-top')) { 
+
+      topBar.classList.remove('bar-active-top-reverse');
+      middleBar.classList.remove('bar-active-middle-reverse');
+      bottomBar.classList.remove('bar-active-bottom-reverse');
+    }
+    else if (topBar.classList.contains('bar-active-top')) {
+        topBar.classList.add('bar-active-top-reverse');
+        middleBar.classList.add('bar-active-middle-reverse');
+        bottomBar.classList.add('bar-active-bottom-reverse');
+
+
+
+    }
+
+    else {
+        topBar.classList.add('bar-active-top');
+        middleBar.classList.add('bar-active-middle');
+        bottomBar.classList.add('bar-active-bottom');
+
+    }
+})
+
+
+
+// Close Menu
+// document.addEventListener('click', function(event) {
+//     if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
+//         menu.classList.remove('active');
+
+//         topBar.classList.remove('bar-active-top');
+//         middleBar.classList.remove('bar-active-middle');
+//         bottomBar.classList.remove('bar-active-bottom');
+
+//         topBar.classList.remove('bar-active-top-reverse');
+//         middleBar.classList.remove('bar-active-middle-reverse');
+//         bottomBar.classList.remove('bar-active-bottom-reverse');
+//     }
+// });
+
